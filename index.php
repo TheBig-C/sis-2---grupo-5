ebbb10d201d8d6d29dd393981a3f0992d7a89834
Jhuly1215 <jhuliannnavitoria@gmail.com>
2024-04-24 23:28:54 -0400

    narvar done, style done

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ketal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="frontend/css/style.css">
+</head>
+<body>
+    <header class="header">
+        <nav class="navbar">
+            <img src="frontend/assets/ketal.png">
+            <a href="productos.php"><b>Productos</b></a>
+            <span> | </span>
+            <a href="ventas.php"><b>Ventas</b></a>
+        </nav>
+        <a href="index.php"><b>Iniciar Sesion</b></a>
+    </header>
+</body>
+</html>
