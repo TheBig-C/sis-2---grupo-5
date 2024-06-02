a82a4900c721940efd95125e8541b99c7f361b4f
Cesar Vera <soymateovera@gmail.com>
2024-06-02 19:10:16 -0400

    dss
    integracion de los dashboards

--- frontend/views/pagina_principal/pagina_opciones.php
+++ frontend/views/pagina_principal/pagina_opciones.php
@@ -16,7 +16,7 @@
             display: flex;
             position: fixed;
             top: 0;
-            left:0;
+            left: 0;
             width: 100%;
             padding: 0vh 5vh;
             background: aliceblue;
@@ -26,7 +26,7 @@
         }
         .header a {
             color: #fff;
-            font-weight: 30;
+            font-weight: bold;
             text-decoration: none;
             font-size: 3vh;
             background-color: #ef233c;
@@ -47,11 +47,11 @@
             height: auto;
         }
         .container.mt-5 {
-            padding-top: 50px; 
+            padding-top: 60px; 
             margin-top: 0;
         }
         .btn-option {
-            height: 150px;
+            height: 100px;
             margin: 10px;
             font-size: 1.5em;
             background-color: #ef233c;
@@ -61,6 +61,7 @@
             justify-content: center; 
             align-items: center;
             text-align: center; 
+            width: calc(33.333% - 20px);
         }
         .btn-option:hover {
             background-color: #d90429; 
@@ -68,11 +69,7 @@
         }
         @media (min-width: 768px) {
             .btn-option {
-                height: calc(50vh - 70px);
-                width: calc(33.333% - 20px);
-            }
-            .btn-option-large {
-                width: calc(25% - 20px);
+                height: calc(25vh - 20px);
             }
         }
     </style>
@@ -82,32 +79,33 @@
         <nav class="navbar">
             <img src="/sis2-Ketal//frontend/assets/ketal.png">
             <?php
-include_once 'C:\xampp\htdocs\sis2-ketal\backend\models\classes.php';
-
-            if (isset($_COOKIE['sucursal'])) {
-                $serializedSucursal = $_COOKIE['sucursal'];
-                $suc = unserialize($serializedSucursal);
-
-            }
-            echo "<span>Menú de Opciones - Sucursal: ". $suc->getZona() ." </span>"; // PHP echo for "Menú de Opciones"
-            ?>        </nav>
+                include_once 'C:\xampp\htdocs\sis2-ketal\backend\models\classes.php';
+                if (isset($_COOKIE['sucursal'])) {
+                    $serializedSucursal = $_COOKIE['sucursal'];
+                    $suc = unserialize($serializedSucursal);
+                }
+                echo "<span>Menú de Opciones - Sucursal: ". $suc->getZona() ." </span>";
+            ?>
+        </nav>
         <a href="../auth/login.html"><b>Salir</b></a>
     </header>
+    <br>
+    <br>
     <div class="container mt-5">
         <div class="row text-center options-container">
-            <!-- Botones de las opciones en la primera fila -->
-            <a href="../ventas/view_realizar_venta.php" class="btn btn-option btn-option-large">REGISTRAR VENTA</a>
-            <a href="../pedido/view_pedido.php" class="btn btn-option btn-option-large">REALIZAR PEDIDO</a>
-            <a href="../productos/view_register_product.php" class="btn btn-option btn-option-large">AGREGAR NUEVO PRODUCTO</a>
-
-            <a href="../funcionario/view_funcionarios.php" class="btn btn-option btn-option-large">EMPLEADOS</a>
+            <a href="../ventas/view_realizar_venta.php" class="btn btn-option">REGISTRAR VENTA</a>
+            <a href="../pedido/view_pedido.php" class="btn btn-option">REALIZAR PEDIDO</a>
+            <a href="../productos/view_register_product.php" class="btn btn-option">AGREGAR NUEVO PRODUCTO</a>
         </div>
         <div class="row text-center options-container">
-            <!-- Botones de las opciones en la segunda fila -->
-            <a href="../funcionario/view_fun.php" class="btn btn-option btn-option-large">AGREGAR EMPLEADO</a>
-            <a href="../estado_pedido/view_epedido.php" class="btn btn-option btn-option-large">PEDIDOS</a>
-            <a href="../inventario/view_inventario.php" class="btn btn-option btn-option-large">INVENTARIO</a>
-            <a href="../ventas/view_ventas.php" class="btn btn-option btn-option-large">VENTAS</a>
+            <a href="../funcionario/view_funcionarios.php" class="btn btn-option">EMPLEADOS</a>
+            <a href="../funcionario/view_fun.php" class="btn btn-option">AGREGAR EMPLEADO</a>
+            <a href="../estado_pedido/view_epedido.php" class="btn btn-option">PEDIDOS</a>
+        </div>
+        <div class="row text-center options-container">
+            <a href="../inventario/view_inventario.php" class="btn btn-option">INVENTARIO</a>
+            <a href="../ventas/view_ventas.php" class="btn btn-option">VENTAS</a>
+            <a href="view_dashboards.html" class="btn btn-option">VER DASHBOARDS</a>
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
